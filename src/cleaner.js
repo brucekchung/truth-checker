5b7acc6648ae22c89791b8b07b24c059fa0483ad
Bruce Chung <brucekchung@gmail.com>
2018-02-20 22:30:02 -0700

    Aggregate scores calulate and post

--- src/cleaner.js
+++ src/cleaner.js
@@ -42,4 +42,20 @@ export const cleanAuthor = (authorData) => {
   }
 }
 
+export const cleanSiteRating = (site) => {
+  if (!site.length) {
+    return 'no matches found'
+
+  } else {
+    const match = site[0]
+    const ratingImage = match.RatingIcons[0].Url
+    const split = ratingImage.split('-')
+    const separate = split[1].split('.')
+    const rating = separate[0]
+
+    console.log('rating: ', rating)
+    return rating ? rating : 'no rating'
+  }
+}
+
 
